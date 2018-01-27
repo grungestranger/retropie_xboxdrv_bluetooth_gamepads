95e34c909aec1744d93a4b49ec565e7cf5a2b3a3
grungestranger <grungestranger@yandex.ru>
2018-01-27 20:27:49 +0300

    master 2018-01-27

--- home/pi/gamepads.sh
+++ home/pi/gamepads.sh
@@ -18,6 +18,7 @@ base="/opt/retropie/supplementary/xboxdrv/bin/xboxdrv > /dev/null 2>&1
 declare -A gamepads
 gamepads['PG-9062S']="--evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_Z=x2,ABS_RZ=y2,ABS_BRAKE=lt,ABS_GAS=rt,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y
 	--evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start
+	--trigger-as-button
 	--device-usbid 0:0:0:0"
 gamepads['PG-9037']="--evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_Z=x2,ABS_RZ=y2,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y
 	--evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_TL2=lt,BTN_TR2=rt,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start
@@ -26,10 +27,10 @@ gamepads['PG-9037']="--evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_Z=x2,ABS_RZ=y2,ABS_HAT
 
 # games settings; games - it's ports for which we need add joy2key settings
 declare -A games
-games['quake3']="--ui-axismap x2=REL_X:10,y2=REL_Y:10
+games['quake3']="--ui-axismap x2=REL_X:20,y2=REL_Y:20
 	--ui-buttonmap tl=BTN_LEFT,tr=BTN_RIGHT
 	--ui-axismap x1=KEY_A:KEY_D,y1=KEY_W:KEY_S
-	--ui-buttonmap a=KEY_Q,b=KEY_E,x=KEY_R,y=KEY_T,lb=KEY_Y,rb=KEY_U,lt=KEY_I,rt=KEY_O,start=KEY_ENTER,back=KEY_ESC
+	--ui-buttonmap a=KEY_END,b=KEY_RIGHTBRACE,x=KEY_LEFTBRACE,y=KEY_TAB,lb=KEY_LEFTSHIFT,rb=KEY_C,lt=KEY_SPACE,rt=KEY_LEFTCTRL,start=KEY_ENTER,back=KEY_ESC
 	--ui-buttonmap du=KEY_UP,dd=KEY_DOWN,dl=KEY_LEFT,dr=KEY_RIGHT"
 
 # kill proc; use global var gamepad
